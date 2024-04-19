f2cff401274c4ec2419430d0d3fe9fe7eb8f3b53
Wail Anwer Mebarkia <166451808+Anwerwail@users.noreply.github.com>
2024-04-19 10:03:29 +0200

    add one qoute index.js

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const quotes = [
   "Stay hungry, stay foolish. - Steve Jobs",
   "The greatest glory in living lies not in never falling, but in rising every time we fall. - Nelson Mandela",
   "The way to get started is to quit talking and begin doing. - Walt Disney",
+  "If you want to buy something without looking to the price you should work without looking to the clock. - A wise man",
   "Don't watch the clock; do what it does. Keep going. - Sam Levenson",
   "The only limit to our realization of tomorrow will be our doubts of today. - Franklin D. Roosevelt",
   "It does not matter how slowly you go as long as you do not stop. - Confucius",
-  "Believe you can and you're halfway there. - Theodore Roosevelt",
-  "If you want to buy something without looking to the price you should work without looking to the clock. - A wise man"
+  "Believe you can and you're halfway there. - Theodore Roosevelt"
 ];
 
 const quoteElement = document.getElementById("quote");
